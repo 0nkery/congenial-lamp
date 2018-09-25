04aa5d25876c60aef40378a6595d9fdf9352b6a3
Dmitry Shlagoff <richyannet@yandex.ru>
2018-09-25 23:58:16 +0700

    Make requests using WeatherAPI trait + testing trash

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate futures;
 extern crate reqwest;
+extern crate tokio;
 
 extern crate chrono;
 extern crate itertools;
@@ -9,13 +10,64 @@ extern crate smallvec;
 extern crate serde_derive;
 extern crate serde;
 
+use apis::WeatherAPI;
+use futures::Future;
+
 mod apis;
+
+fn prepare_request<A, R>(
+    client: &reqwest::async::Client,
+    city: &str,
+    country: &str,
+    api: A,
+) -> impl Future<Item = Option<apis::WeatherDataVec>, Error = reqwest::Error>
+where
+    A: apis::WeatherAPI<Response = R>,
+    R: Into<Option<apis::WeatherDataVec>>,
+    for<'de> R: serde::Deserialize<'de>,
+{
+    let url = api.weekly_request_url(city, country).unwrap();
+    let builder = client.request(A::METHOD, url);
+
+    api.build_weekly_request(builder, city, country)
+        .send()
+        .and_then(|mut res| res.json::<R>())
+        .map(|res| res.into())
+}
 
 fn main() -> Result<(), Box<std::error::Error>> {
     let aeris_api = apis::aerisweather::AerisWeather::new()?;
     let apixu_api = apis::apixu::Apixu::new()?;
     let openweathermap_api = apis::openweathermap::OpenWeatherMap::new()?;
     let weatherbit_api = apis::weatherbit::WeatherBit::new()?;
+
+    let client = reqwest::async::Client::new();
+
+    let req1 = prepare_request(&client, "Novokuznetsk", "RU", aeris_api)
+        .map(|res| println!("{:?}", res))
+        .map_err(|err| println!("{}", err));
+
+    let req2 = prepare_request(&client, "Novokuznetsk", "RU", openweathermap_api)
+        .map(|res| println!("{:?}", res))
+        .map_err(|err| println!("{}", err));
+
+    let req3 = prepare_request(&client, "Novokuznetsk", "RU", apixu_api)
+        .map(|res| println!("{:?}", res))
+        .map_err(|err| println!("{}", err));
+
+    let req4 = prepare_request(&client, "Novokuznetsk", "RU", weatherbit_api)
+        .map(|res| println!("{:?}", res))
+        .map_err(|err| println!("{}", err));
+
+    let join = req1
+        .join(req2)
+        .map(|(res1, res2)| ())
+        .join(req3)
+        .map(|_| ())
+        .join(req4)
+        .map(|_| ());
+
+    tokio::run(join);
 
     Ok(())
 }
