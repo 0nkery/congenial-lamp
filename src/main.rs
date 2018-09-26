1a3ded820933fda693eef8854d9c6e699e1d103e
Dmitry Shlagoff <richyannet@yandex.ru>
2018-09-26 23:43:43 +0700

    Use new api module paths + show aggregated result

--- src/main.rs
+++ src/main.rs
@@ -35,36 +35,38 @@ where
 }
 
 fn main() -> Result<(), Box<std::error::Error>> {
-    let aeris_api = apis::aerisweather::AerisWeather::new()?;
-    let apixu_api = apis::apixu::Apixu::new()?;
-    let openweathermap_api = apis::openweathermap::OpenWeatherMap::new()?;
-    let weatherbit_api = apis::weatherbit::WeatherBit::new()?;
+    let aeris_api = apis::AerisWeather::new()?;
+    let apixu_api = apis::Apixu::new()?;
+    let openweathermap_api = apis::OpenWeatherMap::new()?;
+    let weatherbit_api = apis::WeatherBit::new()?;
 
     let client = reqwest::async::Client::new();
 
     let req1 = prepare_request(&client, "Novokuznetsk", "RU", aeris_api)
-        .map(|res| println!("{:?}", res))
         .map_err(|err| println!("{}", err));
 
     let req2 = prepare_request(&client, "Novokuznetsk", "RU", openweathermap_api)
-        .map(|res| println!("{:?}", res))
         .map_err(|err| println!("{}", err));
 
     let req3 = prepare_request(&client, "Novokuznetsk", "RU", apixu_api)
-        .map(|res| println!("{:?}", res))
         .map_err(|err| println!("{}", err));
 
     let req4 = prepare_request(&client, "Novokuznetsk", "RU", weatherbit_api)
-        .map(|res| println!("{:?}", res))
         .map_err(|err| println!("{}", err));
 
     let join = req1
         .join(req2)
-        .map(|_| ())
+        .map(apis::join_two_vecs)
         .join(req3)
-        .map(|_| ())
+        .map(apis::join_two_vecs)
         .join(req4)
-        .map(|_| ());
+        .map(apis::join_two_vecs)
+        .map(|vec| {
+            if let Some(vec) = vec {
+                let aggregate = apis::aggregate_results(vec);
+                println!("{:?}", aggregate);
+            }
+        });
 
     tokio::run(join);
 
