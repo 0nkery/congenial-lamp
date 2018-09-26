16c41f31cf0e8ae23ad6928ef25ab56e832f4f2d
Dmitry Shlagoff <richyannet@yandex.ru>
2018-09-26 23:42:51 +0700

    use -> pub use + add convenient functions to join vecs && aggregate result

--- src/apis/mod.rs
+++ src/apis/mod.rs
@@ -1,17 +1,23 @@
-pub mod aerisweather;
-pub mod apixu;
-pub mod openweathermap;
-pub mod weatherbit;
+mod aerisweather;
+mod apixu;
+mod openweathermap;
+mod weatherbit;
+
+pub use self::aerisweather::AerisWeather;
+pub use self::apixu::Apixu;
+pub use self::openweathermap::OpenWeatherMap;
+pub use self::weatherbit::WeatherBit;
 
 use chrono::{Date, Utc};
+use itertools::Itertools;
 use reqwest::async::RequestBuilder;
 use reqwest::{Method, Url, UrlError};
 use smallvec::SmallVec;
 
 #[derive(Debug)]
 pub struct WeatherData {
-    temperature: f32,
-    date: Date<Utc>,
+    pub temperature: f32,
+    pub date: Date<Utc>,
 }
 
 pub type WeatherDataVec = SmallVec<[WeatherData; 32]>;
@@ -31,4 +37,39 @@ pub trait WeatherAPI {
     ) -> RequestBuilder {
         req_builder
     }
+}
+
+pub fn join_two_vecs(
+    (vec1, vec2): (Option<WeatherDataVec>, Option<WeatherDataVec>),
+) -> Option<WeatherDataVec> {
+    match (vec1, vec2) {
+        (Some(mut vec1), Some(vec2)) => {
+            vec1.extend(vec2);
+            Some(vec1)
+        }
+        (None, Some(vec)) => Some(vec),
+        (Some(vec), None) => Some(vec),
+        _ => None,
+    }
+}
+
+pub fn aggregate_results(mut weather_data: WeatherDataVec) -> WeatherDataVec {
+    weather_data.sort_unstable_by(|entry1, entry2| entry1.date.cmp(&entry2.date));
+
+    weather_data
+        .iter()
+        .group_by(|entry| entry.date)
+        .into_iter()
+        .map(|(day, data)| {
+            let (temperature_sum, points_count) = data.fold((0.0, 0.0), |(sum, count), data| {
+                (sum + data.temperature, count + 1.0)
+            });
+
+            let avg_temperature = temperature_sum / points_count;
+
+            WeatherData {
+                date: day,
+                temperature: avg_temperature,
+            }
+        }).collect::<WeatherDataVec>()
 }
